7a85ddda435cfce7e38dabe934e7ed1eeae64ce4
Kenzo Mendoza <kenzom954@gmail.com>
2016-09-13 19:22:24 -0700

    add generation text

---
+++ src/components/generation.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+export default class Generation extends Component {
+  render(){
+    return(
+      <h3 className="center-align">Generation:</h3>
+    );
+  }
+}
