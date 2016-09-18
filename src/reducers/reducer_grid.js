9a2209c3844fe1db6f64156b585404ff52bd0996
Kenzo Mendoza <kenzom954@gmail.com>
2016-09-17 21:57:28 -0700

    collect all 8 neighbors

--- src/reducers/reducer_grid.js
+++ src/reducers/reducer_grid.js
@@ -13,12 +13,34 @@ const initialState = {
 };
 
 initialState.cells = Array.from(
-  {length: initialState.width * initialState.height}, () => getRandomInt(0, 1))
+  {length: initialState.width * initialState.height}, () => getRandomInt(0, 1)
+)
+function neighbors(pos,dimension){
+  let neighbors = []
+  const width = dimension[0];
+  const height = dimension[1];
+  //coord are coordinates in array: [y coordinate, x coordinate]
+  let coord = pos.split("-").map(coord => parseInt(coord))
+  //lets check all boundries - if it's at edge, crossover the otherside
+  const upBoundary = coord[0] === 0 ? height - 1 : coord[0] - 1;
+  const downBoundary = coord[0] === height - 1 ? 0 : coord[0] + 1;
+  const leftBoundary = coord[1] === 0 ? width - 1 : coord[1] - 1;
+  const rightBoundary = coord[1] === width - 1 ? 0 : coord[1] + 1;
+  //lets gather all the 8-neighbors
+  neighbors.push([upBoundary,coord[1]].join("-")) //top
+  neighbors.push([upBoundary,rightBoundary].join("-")) //top-right
+  neighbors.push([coord[0],rightBoundary].join("-")) //right
+  neighbors.push([downBoundary,rightBoundary].join("-")) //bottom-right
+  neighbors.push([downBoundary,coord[1]].join("-")) //bottom
+  neighbors.push([downBoundary,leftBoundary].join("-")) //bottom-left
+  neighbors.push([coord[0],leftBoundary].join("-")) //left
+  neighbors.push([upBoundary,leftBoundary].join("-")) //top-left
+  console.log(neighbors)
+}
 
-function nextGeneration(currentGeneration){
-  for (var prop in currentGeneration){
-    console.log(prop)
-    console.log(currentGeneration[prop])
+function nextGeneration(currentGeneration,state){
+  for (var coordinate in currentGeneration){
+    neighbors(coordinate,[state.width,state.height])
   }
 
 }
@@ -36,7 +58,7 @@ export default function(state = initialState, action){
         cells
       };
     case START:
-      nextGeneration(action.payload)
+      nextGeneration(action.payload,state)
       return state
 
     case CLEAR:
